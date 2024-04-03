608e73f6abaa472ed7e562cae782ebe3cc26fea6
Adam SOIMANSOIB <adamsoimansoibpro@gmail.com>
2024-04-03 11:19:52 +0200

    [test] Test WindowStack behavior
    Create unit test and interactive test to ensure WindowStack behave properly.

---
+++ tests/interactive_tests/test_window_stack_interactive.py
@@ -0,0 +1,46 @@
+from pyimgui_utils.window import WindowStackOrientation, WindowStack
+from tests.interactive_tests.utils import run_test_window
+from tests.utils import DummyWindow
+
+
+class TestWindowStackInteractive:
+
+    def test_window_stack_vertical(self):
+        wnds = [DummyWindow() for _ in range(3)]
+        offset = 8
+        orientation = WindowStackOrientation.VERTICAL
+        wd_ut = WindowStack(
+            wnds=wnds,
+            offset=offset,
+            orientation=orientation
+        )
+        run_test_window(
+            window_under_test=wd_ut,
+            expected_behavior=["You should see a column of 3 windows, aligned ",
+                               "on the the left segment and stacked from top "
+                               "to bottom.",
+                               "You must observer a slight padding between "
+                               "windows",
+                               "Moving the first window on the top move every "
+                               "stacked windows."]
+        )
+
+    def test_window_stack_horizontal(self):
+        wnds = [DummyWindow() for _ in range(3)]
+        offset = 8
+        orientation = WindowStackOrientation.HORIZONTAL
+        wd_ut = WindowStack(
+            wnds=wnds,
+            offset=offset,
+            orientation=orientation
+        )
+        run_test_window(
+            window_under_test=wd_ut,
+            expected_behavior=["You should see a column of 3 windows, aligned ",
+                               "on the the top segment and stacked from left "
+                               "to right.",
+                               "You must observer a slight padding between "
+                               "windows",
+                               "Moving the first window on the left move every "
+                               "stacked windows."]
+        )
--- tests/test_window.py
+++ tests/test_window.py
@@ -4,7 +4,9 @@ import pytest
 from typing_extensions import override
 
 from pyimgui_utils import ImGuiWindowAbstract
-from tests.utils import setup_imgui_context
+from pyimgui_utils.window import WindowStack, WindowStackOrientation
+from tests.utils import (setup_imgui_context, FixedSizeWindow,
+                         terminate_imgui_context)
 
 
 class Window(ImGuiWindowAbstract):
@@ -32,7 +34,7 @@ class TestImGuiWindowAbstract():
             ImGuiWindowAbstract()
 
     def test_before_after_begin_end_methods(self):
-        setup_imgui_context()
+        impl, _, ctx = setup_imgui_context()
 
         class ClassUnderTest(Window):
             def __init__(self):
@@ -55,5 +57,57 @@ class TestImGuiWindowAbstract():
         imgui.new_frame()
         cls_ut.draw()
         imgui.render()
+        terminate_imgui_context(impl, ctx)
 
-        assert cls_ut.call_order == [1, 2, 3, 4], "Function call order not respected"
+        assert cls_ut.call_order == [1, 2, 3, 4], \
+            "Function call order not respected"
+
+
+class TestWindowStack:
+    small_wnd_size = (40, 50)
+    small_wnd_nb = 3
+    big_wnd_size = (60, 80)
+    big_wnd_nb = 1
+    offset = 12
+
+    def test_window_stack_size(self):
+        impl, _, ctx = setup_imgui_context()
+
+        fixed_size_wnds = [FixedSizeWindow(*self.small_wnd_size)
+                           for _ in range(self.small_wnd_nb)]
+        fixed_size_wnds.append(FixedSizeWindow(*self.big_wnd_size))
+
+        horizontal_stack = WindowStack(
+            wnds=fixed_size_wnds,
+            orientation=WindowStackOrientation.HORIZONTAL,
+            offset=self.offset
+        )
+
+        vertical_stack = WindowStack(
+            wnds=fixed_size_wnds,
+            orientation=WindowStackOrientation.VERTICAL,
+            offset=self.offset
+        )
+
+        imgui.new_frame()
+        horizontal_stack.draw()
+        vertical_stack.draw()
+        imgui.render()
+        terminate_imgui_context(impl, ctx)
+
+        expected_offset_sum = 12 * 3
+        expected_wnd_width_sum = (self.small_wnd_size[0] * self.small_wnd_nb
+                                  + self.big_wnd_size[0])
+        expected_width = expected_wnd_width_sum + expected_offset_sum
+        expected_height = self.big_wnd_size[1]
+        expected_size = (expected_width, expected_height)
+        assert horizontal_stack.size == expected_size, \
+            "Expected horizontal stack size do not match with actual one."
+
+        expected_wnd_height_sum = (self.small_wnd_size[1] * self.small_wnd_nb
+                                   + self.big_wnd_size[1])
+        expected_height = expected_wnd_height_sum + expected_offset_sum
+        expected_width = self.big_wnd_size[0]
+        expected_size = (expected_width, expected_height)
+        assert vertical_stack.size == expected_size, \
+            "Expected vertical stack size do not match with actual one."
